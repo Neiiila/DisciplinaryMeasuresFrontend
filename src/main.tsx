a6d8b5b85fe84299273225eca8053be682b880e0
Naila Bouadil <nbouadil@gmail.com>
2026-08-30 03:43:45 +0100

    Replace starter scaffold with the application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
